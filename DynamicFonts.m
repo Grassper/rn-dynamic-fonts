1bc5b00814afe627ff17c4ffef0501f3f7b5fedd
vadivazhagan-vadivel <vadivazhagan.v@apptile.io>
2022-08-05 10:58:54 +0530

    docs: license info updated

--- DynamicFonts.m
+++ DynamicFonts.m
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2017-present, Wyatt Greenway. All rights reserved.
+ * Copyright (c) 2017-present, Apptile.io. All rights reserved.
  *
  * This source code is licensed under the MIT license found in the LICENSE file in the root
  * directory of this source tree.
