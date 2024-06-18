43bbb06aafa5d61858d46fd9689345fcf8ee8182
vadivazhagan-vadivel <vadivazhagan.v@apptile.io>
2024-06-18 13:15:27 +0530

    nil injection raising while giving invalid font fixed

--- DynamicFonts.m
+++ DynamicFonts.m
@@ -4,7 +4,6 @@
  * This source code is licensed under the MIT license found in the LICENSE file in the root
  * directory of this source tree.
  */
-
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #import <CoreText/CoreText.h>
@@ -17,32 +16,29 @@ RCT_EXPORT_MODULE();
 - (void) loadFontWithData:(CGDataProviderRef) fontDataProvider callback:(RCTResponseSenderBlock)callback
 {
   CGFontRef newFont = CGFontCreateWithDataProvider(fontDataProvider);
-  NSString *newFontName = (__bridge NSString *)CGFontCopyPostScriptName(newFont);
+  NSString *newFontName = (__bridge_transfer NSString *)CGFontCopyPostScriptName(newFont);
 
-  UIFont* font = [UIFont fontWithName:newFontName size:16];
-  if (font != nil) {
+  if (newFontName && [UIFont fontWithName:newFontName size:16]) {
+    CGFontRelease(newFont);
     CGDataProviderRelease(fontDataProvider);
     callback(@[[NSNull null], newFontName]);
     return;
   }
 
   CFErrorRef error;
-  if (! CTFontManagerRegisterGraphicsFont(newFont, &error)) {
+  if (!CTFontManagerRegisterGraphicsFont(newFont, &error)) {
     CFStringRef errorDescription = CFErrorCopyDescription(error);
     NSLog(@"Failed to register font: %@", errorDescription);
-
-    callback(@[@"Failed to register font: %@", (__bridge NSString *)errorDescription]);
-  
+    callback(@[[NSString stringWithFormat:@"Failed to register font: %@", (__bridge_transfer NSString *)errorDescription]]);
     CFRelease(errorDescription);
+    CFRelease(error);
     CGFontRelease(newFont);
     CGDataProviderRelease(fontDataProvider);
-    
     return;
   }
 
   CGFontRelease(newFont);
   CGDataProviderRelease(fontDataProvider);
-
   callback(@[[NSNull null], newFontName]);
 }
 
@@ -50,45 +46,42 @@ RCT_EXPORT_METHOD(loadFont:(NSDictionary *)options callback:(RCTResponseSenderBl
 {
   NSString *name = [options valueForKey:@"name"];
   NSString *data = [options valueForKey:@"data"];
-  NSString *type = NULL;
+  NSString *type = [options valueForKey:@"type"];
   
-  if ([name isEqual:[NSNull null]]) {
+  if ([name isEqual:[NSNull null]] || [name length] == 0) {
     callback(@[@"Name property is empty"]);
     return;
   }
 
-  if ([data isEqual:[NSNull null]]) {
+  if ([data isEqual:[NSNull null]] || [data length] == 0) {
     callback(@[@"Data property is empty"]);
     return;
   }
 
   if ([[[data substringWithRange:NSMakeRange(0, 5)] lowercaseString] isEqualToString:@"data:"]) {
     NSArray *parts = [data componentsSeparatedByString:@","];
-    NSString *mimeType = [parts objectAtIndex:0];
+    NSString *mimeType = parts[0];
+    data = parts[1];
 
-    data = [parts objectAtIndex:1];
-
-    if (![mimeType isEqual:[NSNull null]]) {
+    if (![mimeType isEqual:[NSNull null]] && [mimeType length] > 0) {
       mimeType = [[[mimeType substringFromIndex:5] componentsSeparatedByString:@";"] objectAtIndex:0];
 
       if ([mimeType isEqualToString:@"application/x-font-ttf"] || 
           [mimeType isEqualToString:@"application/x-font-truetype"] ||
           [mimeType isEqualToString:@"font/ttf"]) {
         type = @"ttf";
-      } else if ( [mimeType isEqualToString:@"application/x-font-opentype"] || 
-                  [mimeType isEqualToString:@"font/opentype"]) {
+      } else if ([mimeType isEqualToString:@"application/x-font-opentype"] || 
+                 [mimeType isEqualToString:@"font/opentype"]) {
         type = @"otf";
       }
     }
   }
 
-  if ([type isEqual:[NSNull null]])
-    type = [options valueForKey:@"type"];
-
-  if ([type isEqual:[NSNull null]])
+  if ([type isEqual:[NSNull null]] || [type length] == 0) {
     type = @"ttf";
-    
-  NSData *decodedData = [[NSData alloc]initWithBase64EncodedString:data options:NSDataBase64DecodingIgnoreUnknownCharacters];
+  }
+
+  NSData *decodedData = [[NSData alloc] initWithBase64EncodedString:data options:NSDataBase64DecodingIgnoreUnknownCharacters];
   CGDataProviderRef fontDataProvider = CGDataProviderCreateWithCFData((__bridge CFDataRef)decodedData);
   [self loadFontWithData:fontDataProvider callback:callback];
 }
@@ -97,24 +90,33 @@ RCT_EXPORT_METHOD(loadFontFromFile:(NSDictionary *)options callback:(RCTResponse
 {
   NSString *name = [options valueForKey:@"name"];
   NSString *filePath = [options valueForKey:@"filePath"];
-  NSString *type = NULL;
+  NSString *type = [options valueForKey:@"type"];
   
-  if ([name isEqual:[NSNull null]]) {
+  if ([name isEqual:[NSNull null]] || [name length] == 0) {
     callback(@[@"Name property is empty"]);
     return;
   }
 
-  if ([filePath isEqual:[NSNull null]]) {
+  if ([filePath isEqual:[NSNull null]] || [filePath length] == 0) {
     callback(@[@"FilePath property is empty"]);
     return;
   }
 
-  if ([type isEqual:[NSNull null]])
-    type = [options valueForKey:@"type"];
+  if ([type isEqual:[NSNull null]] || [type length] == 0) {
+    type = @"ttf";
+  }
 
-  if ([type isEqual:[NSNull null]]) type = @"ttf";
-    
+  if (![[NSFileManager defaultManager] fileExistsAtPath:filePath]) {
+    callback(@[@"Font file does not exist"]);
+    return;
+  }
+
   CGDataProviderRef fontDataProvider = CGDataProviderCreateWithFilename(filePath.fileSystemRepresentation);
+  if (!fontDataProvider) {
+    callback(@[@"Failed to create font data provider"]);
+    return;
+  }
+
   [self loadFontWithData:fontDataProvider callback:callback];
 }
 
